18d42eda76eb961a16756223cb78db6f1c93b241
dit03 <dit@imac-4.local>
2019-04-04 15:15:20 +0900

    New Source

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -5,37 +5,43 @@ class ViewController: UIViewController {
 
     var mTimer = Timer()
     
-    var hour = 0, minute = 0, second = 0
+    var hour = 0, minute = 0, second = 0, cSecond = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     func calcTime() {
-        if (second != 59) {
-            second += 1;
+        if (cSecond != 99) {
+            cSecond += 1;
         } else {
-            second = 0;
+            cSecond = 0
             
-            if (minute != 59) {
-                minute += 1;
+            if (second != 59) {
+                second += 1;
             } else {
-                minute = 0;
+                second = 0;
                 
-                if (hour != 99) {
-                    hour += 1;
+                if (minute != 59) {
+                    minute += 1;
                 } else {
-                    hour = 0;
+                    minute = 0;
+                    
+                    if (hour != 99) {
+                        hour += 1;
+                    } else {
+                        hour = 0;
+                    }
                 }
             }
         }
         
-        clock.text = (hour < 10 ? "0" + String(hour) : String(hour)) + ":" + (minute < 10 ? "0" + String(minute) : String(minute)) + ":" + (second < 10 ? "0" + String(second) : String(second))
+        clock.text = (hour < 10 ? "0" + String(hour) : String(hour)) + ":" + (minute < 10 ? "0" + String(minute) : String(minute)) + ":" + (second < 10 ? "0" + String(second) : String(second)) + "." + (cSecond < 10 ? "0" + String(cSecond) : String(cSecond))
     }
     
     @IBAction func play(_ sender: Any) {
         if (!mTimer.isValid) {
-            mTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in self.calcTime() })
+            mTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { timer in self.calcTime() })
         }
     }
     
@@ -50,6 +56,6 @@ class ViewController: UIViewController {
         minute = 0;
         second = 0;
         
-        clock.text = "00:00:00"
+        clock.text = "00:00:00.00"
     }
 }
